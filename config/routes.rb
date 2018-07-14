e97839b1d6b0e8515cd57627c4c0a4ebeb0b90e9
jhon <jhone.cortesg@gmail.com>
2018-07-14 11:33:49 -0500

    Crear pagina de inicio
    - Borramos las rutas de User y Page
    - Agregamos ruta para el home de la aplicación

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+  #resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get to: 'page#show'
+  #get to: 'page#show'
 
   root to: "page#index"
 end
